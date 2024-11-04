bc7d357fb8d38623cfecf6a885e2567fff0ef097
Rajia Sultana Shraboni <shraboni3103@gmail.com>
2024-11-04 20:24:42 +0600

    boolean type variable is here

--- variable.js
+++ variable.js
@@ -1,3 +1,5 @@
+// number type variable
+
 var age = 36;
 var price = 125;
 var money = 2005;
@@ -5,11 +7,20 @@ var money = 2005;
 age = 35;
 
 var friend;
-console.log(friend);
+// console.log(friend);
 
-console.log(age);
+console.log(typeof age);
+
+// string type variable
 
 var name = "sharboni";
 var place = "Shundor ban ";
 
-console.log(place);
+console.log(typeof place);
+
+// Boolean type variable
+
+var isRich = true;
+var passed = false;
+
+console.log(typeof isRich);
