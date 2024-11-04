d21bee3776e327058b9b7b79a9292fc6d8463f90
Rajia Sultana Shraboni <shraboni3103@gmail.com>
2024-11-04 19:51:01 +0600

    how to declare string is practices here

--- variable.js
+++ variable.js
@@ -2,7 +2,14 @@ var age = 36;
 var price = 125;
 var money = 2005;
 
+age = 35;
+
 var friend;
 console.log(friend);
 
 console.log(age);
+
+var name = "sharboni";
+var place = "Shundor ban ";
+
+console.log(place);
