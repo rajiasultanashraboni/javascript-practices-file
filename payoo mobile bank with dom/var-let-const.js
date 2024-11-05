bd626331c0f2ee77b12a6dbab0b642567e3630a7
Rajia Sultana Shraboni <shraboni3103@gmail.com>
2024-11-05 12:11:14 +0600

    mathematical operator shorthand is here

--- payoo mobile bank with dom/var-let-const.js
+++
@@ -1,3 +0,0 @@
-const sum = 50;
-sum = 30;
-console.log(sum);
---
+++ shorthand.js
@@ -0,0 +1,37 @@
+// addition shorthand
+var price = 20;
+// var price2 = price + 10;
+
+// etar mane holo price er bortoman man er sathe 10 jug kora
+
+price += 10;
+
+console.log(price);
+
+// substraction shorthand
+
+var num = 205;
+num -= 5;
+
+console.log(num);
+
+// multiple shorthand
+
+var buy = 155;
+buy *= 2;
+console.log(buy);
+
+// division shorthand
+
+var age = 34;
+
+age /= 2;
+
+console.log(age);
+
+// modulus shorthand
+
+var jinish = 2000;
+jinish %= 3;
+
+console.log(jinish);
