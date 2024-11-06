3ab02ca93e40932595746d023338546df071aa71
Rajia Sultana Shraboni <shraboni3103@gmail.com>
2024-11-06 21:51:30 +0600

    how to find grade using if else is here

---
+++ grade.js
@@ -0,0 +1,11 @@
+const grade = 75;
+
+if (grade >= 80) {
+  console.log("A+");
+} else if (grade >= 70) {
+  console.log("A");
+} else if (grade >= 60) {
+  console.log("A-");
+} else {
+  console.log("Fail");
+}
