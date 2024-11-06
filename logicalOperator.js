f120c0298cbbdc1600cdad5e481ccb0204de0458
Rajia Sultana Shraboni <shraboni3103@gmail.com>
2024-11-06 19:40:53 +0600

    updated

--- logicalOperator.js
+++ logicalOperator.js
@@ -2,8 +2,14 @@ const salary = 25000;
 const isBcs = true;
 const height = 61;
 
-if (salary > 20000 && height > 65) {
-  console.log("biyer jonno valo patro");
+// if (salary > 20000 && height > 65) {
+//   console.log("biyer jonno valo patro");
+// } else {
+//   console.log("onno patro khujte hobe");
+// }
+
+if (salary > 25000 || height > 72) {
+  console.log("esho baba kobul");
 } else {
-  console.log("onno patro khujte hobe");
+  console.log("vag tui mokbul");
 }
