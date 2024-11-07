807549f97d662c9219df2d1019f3c5bbeb26a623
Rajia Sultana Shraboni <shraboni3103@gmail.com>
2024-11-07 11:51:43 +0600

    logical not operator is here

---
+++ logicalNot.js
@@ -0,0 +1,17 @@
+const isLeader = false;
+
+// if(isLeader===true)
+if (isLeader) {
+  console.log("leader ashche chair charo");
+} else {
+  console.log("ghore dhkte dibi na ");
+}
+
+const isPassed = false;
+
+// if(isPassed===false)
+if (!isPassed) {
+  console.log("tore biya diya dimu");
+} else {
+  console.log("tui amader noyon er moni");
+}
