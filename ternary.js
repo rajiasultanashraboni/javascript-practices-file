c3f69d72dd713ca0fbb05f108b5ff5400a9db22c
Rajia Sultana Shraboni <shraboni3103@gmail.com>
2024-11-07 11:27:45 +0600

    nested ternary operator is here

--- ternary.js
+++ ternary.js
@@ -7,7 +7,7 @@ var age = 20;
 
 // age >= 18 ? console.log("you can vote") : console.log("ghumai thako");
 
-let price = 500;
+let price = 2000;
 let isLeader = false;
 
 if (isLeader === true) {
@@ -16,6 +16,20 @@ if (isLeader === true) {
   price = price + 100;
 }
 
+// console.log(price);
+
+// price = isLeader === true ? 0 : price + 100;
+
+if (isLeader === true) {
+  if (price > 1000) {
+    price = price / 2;
+  } else {
+    price = 0;
+  }
+} else {
+  price = price + 1000;
+}
+
 console.log(price);
 
-price = isLeader === true ? 0 : price + 100;
+price = isLeader === true ? (price > 1000 ? price / 2 : 0) : price + 1000;
