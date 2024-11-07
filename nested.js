ab58d1c302b058bb8cbf26abfec2b9c88dbbdaaf
Rajia Sultana Shraboni <shraboni3103@gmail.com>
2024-11-07 10:56:35 +0600

    nested if else is here

---
+++ nested.js
@@ -0,0 +1,14 @@
+var money = 300;
+if (money > 200) {
+  console.log("bro you are rich");
+} else {
+  if (money > 100) {
+    console.log("tui borolok o na gorib o na ");
+  } else {
+    if (money > 0) {
+      console.log("kola khao calcium barao");
+    } else {
+      console.log("tui amar bondhu na");
+    }
+  }
+}
