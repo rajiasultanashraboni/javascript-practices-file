39d65477a5486244c07fa743e050a64072ac33cc
Rajia Sultana Shraboni <shraboni3103@gmail.com>
2024-11-07 12:16:11 +0600

    conditional homework is here

---
+++ drinks.js
@@ -0,0 +1,13 @@
+/***
+
+Free Drinks
+    - Burger more than 500tk: free Coke
+    - Else Coke: 30tk
+*/
+
+var burger = 400;
+if (burger > 500) {
+  console.log("free coke");
+} else {
+  console.log("30tk dite hobe");
+}
